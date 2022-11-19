d1b3f289f7ffa51dbe0aa75dafef1051060c25c1
Ross P. Coron <rpcoron@gmail.com>
2022-11-19 13:38:18 +0000

    Initial comments

--- network/models.py
+++ network/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+# Inherits from AbstractUser - has access to username, email, password, etc.
 class User(AbstractUser):
     pass
