20f4db93b342c3d55213b441082d0b0bc0d20cd3
Ross P. Coron <rpcoron@gmail.com>
2022-11-22 22:42:29 +0000

    Defined and created 'Tweet' model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,8 @@ from django.db import models
 # Inherits from AbstractUser - has access to username, email, password, etc.
 class User(AbstractUser):
     pass
+
+class Tweet(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweetText = models.CharField(max_length=255)
+    posted = models.DateTimeField(auto_now_add=True)
