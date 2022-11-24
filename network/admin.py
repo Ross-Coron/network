6851c207ea34e6b821e956bc71d2c13a8198598b
Ross P. Coron <rpcoron@gmail.com>
2022-11-24 20:41:18 +0000

    Added models to admin interface

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+# View in admin interface
+from .models import User, Follow, Tweet, Like
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Follow)
+admin.site.register(Tweet)
+admin.site.register(Like)
