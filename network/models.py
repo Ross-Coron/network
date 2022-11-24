b95f5307bcc97c82b61234cadbdac564c2ade920
Ross P. Coron <rpcoron@gmail.com>
2022-11-24 19:59:01 +0000

    New models

--- network/models.py
+++ network/models.py
@@ -5,7 +5,15 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
+    following = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class Tweet(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     tweetText = models.CharField(max_length=255)
     posted = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    likedBy = models.ForeignKey(User, on_delete=models.CASCADE)
