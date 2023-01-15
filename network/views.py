f15dd40a7898000a028fd57e78401013ba2e6f9e
Ross P. Coron <rpcoron@gmail.com>
2023-01-15 18:04:18 +0000

    Fixed follow bug and tidied code

--- network/views.py
+++ network/views.py
@@ -22,7 +22,6 @@ def index(request):
         return HttpResponseRedirect(reverse("login"))
     
 
-
 # Create new post
 @login_required(redirect_field_name='my_redirect_field')   # Redirect to login. Route saved in settings.py
 def tweet(request):
@@ -119,7 +118,6 @@ def following(request):
     # Gets posts from profiles in reverse chronological order
     tweets = Tweet.objects.filter(author__in=usersFollowedIds).order_by('-posted')
     
-
     paginator = Paginator(tweets, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -197,13 +195,20 @@ def follow(request, user_id):
     profile = User.objects.get(pk=user_id)
     print("Debug:", profile, type(profile), profile.following.all())
 
-    # Check if user already follows profile
+    # Check if user already follows profile. If so, remove
     if Follow.objects.filter(user=profile, followed_by=user).exists():        
       
         print("Debug: user currently following profile")
         instance = Follow.objects.filter(user=profile, followed_by=user).get()
 
         user.following.remove(instance)
+
+        # If empty ManyToMany field, delete relationship
+        a = Follow.objects.filter(user=profile)
+        for x in a:
+            if not x.followed_by.exists():
+                x.delete()
+
         print("Debug: user no longer following profile")
 
     else:
@@ -212,7 +217,6 @@ def follow(request, user_id):
         instance = Follow(user=profile)
         instance.save()
 
- 
         instance.followed_by.add(user)
         print("Debug: user now following profile")
 
